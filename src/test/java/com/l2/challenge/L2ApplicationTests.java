954df25f6de35f836eeaf02816676825fed3d81f
Lucas <gianotti.92@gmail.com>
2022-01-07 09:49:47 -0300

    Initial commit
    Added initial scafolding

---
+++ src/test/java/com/l2/challenge/L2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.l2.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
